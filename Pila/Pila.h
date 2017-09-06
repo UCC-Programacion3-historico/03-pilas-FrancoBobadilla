f09b6fd139d82af5ce84150e741f85b132c0e36e
FrancoBobadilla <1709673@ucc.edu.ar>
2017-09-06 11:46:44 -0300

    Prueba de commit and push

--- Pila/Pila.h
+++ Pila/Pila.h
@@ -1,11 +1,6 @@
 #ifndef LISTA_H
 #define LISTA_H
 
-/**
- * Clase que implementa una Pila generica, ya que puede
- * almacenar cualquier tipo de dato T
- * @tparam T cualquier tipo de dato
- */
 template<class T>
 class Pila {
 private:
@@ -23,45 +18,30 @@ public:
 };
 
 
-/**
- * Constructor de la clase Pila
- * @tparam T
- */
 template<class T>
-Pila<T>::Pila() {}
+Pila<T>::Pila() {
+
+}
 
 
-/**
- * Destructor de la clase Lista, se encarga de liberar la memoria de todos los nodos
- * utilizados en la lista
- * @tparam T
- */
 template<class T>
-Pila<T>::~Pila() {}
+Pila<T>::~Pila() {
+
+}
 
 
-/**
- * Inserta un dato en la pila
- * @tparam T
- * @param dato  dato a insertar
- */
 template<class T>
-void Pila<T>::push(T dato) {}
+void Pila<T>::push(T dato) {
+
+}
 
 
-/**
- * Obtener el dato de la pila
- * @tparam T
- * @return dato almacenado en el nodo
- */
 template<class T>
-T Pila<T>::pop() {}
+T Pila<T>::pop() {
 
-/**
- * Responde si la pila se encuentra Vacía
- * @tparam T
- * @return
- */
+}
+
+
 template<class T>
 bool Pila<T>::esVacia() {
 
