27aedff677025100a9db2cae341dcb49bd4b372a
FrancoBobadilla <30866005+FrancoBobadilla@users.noreply.github.com>
2017-09-06 21:22:26 -0300

    9:22 6/9/17  Se genero clases Nodo y Pila

--- Pila/Pila.h
+++ Pila/Pila.h
@@ -1,10 +1,13 @@
-#ifndef LISTA_H
-#define LISTA_H
+#ifndef PILA_H
+#define PILA_H
+
+#include <iostream>
+#include "Nodo.h"
 
 template<class T>
 class Pila {
 private:
-
+    Nodo<T> *inicio;
 public:
     Pila();
 
@@ -15,36 +18,51 @@ public:
     T pop();
 
     bool esVacia();
+
+    T peek();
 };
 
 
 template<class T>
 Pila<T>::Pila() {
-
+    inicio = NULL;
 }
 
 
 template<class T>
 Pila<T>::~Pila() {
-
+    while (!esVacia())pop();
 }
 
 
 template<class T>
 void Pila<T>::push(T dato) {
-
+    Nodo<T> *aux = new Nodo<T>(dato, inicio);
+    inicio = aux;
 }
 
 
 template<class T>
 T Pila<T>::pop() {
-
+    if (inicio == NULL) throw 1;
+    T dato = inicio->getDato();
+    Nodo<T> *aux = inicio;
+    inicio = inicio->getNext();
+    delete aux;
+    return dato;
 }
 
 
 template<class T>
 bool Pila<T>::esVacia() {
-
+    return inicio == NULL;
 }
 
-#endif //LISTA_H
+
+template<class T>
+T Pila<T>::peek() {
+    if (inicio == NULL) throw 1;
+    return inicio->getDato();
+}
+
+#endif //PILA_H
