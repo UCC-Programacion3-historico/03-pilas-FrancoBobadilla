83a1ea5e6b5e2d12a21370c413676118483583e6
alumno <1709673@ucc.edu.ar>
2017-09-07 15:42:32 -0300

    15:42 7/9  Se terminaron los primeros 2 ejercicios

--- Ej-01/main.cpp
+++ Ej-01/main.cpp
@@ -1,7 +1,22 @@
 #include <iostream>
 #include "../Pila/Pila.h"
+#include <string>
+using namespace std;
 
 int main() {
-    std::cout << "Ejercicio 03/01\n" << std::endl;
+    Pila<char> pila;
+
+    string texto;
+    cout<<"Ingrese texto a invertir"<<endl;
+    cin>>texto;
+
+    int fin=texto.length();
+
+    for(int i=0;i<fin;i++)
+        pila.push(texto[i]);
+
+    for(int i=0;!pila.esVacia();i++)
+        cout<<pila.pop();
+
     return 0;
 }
--- Ej-02/main.cpp
+++ Ej-02/main.cpp
@@ -1,7 +1,37 @@
 #include <iostream>
 #include "../Pila/Pila.h"
-
+using namespace std;
+bool sonIguales(Pila<int>,Pila<int>);
 int main() {
-    std::cout << "Ejercicio 03/02\n" << std::endl;
+    Pila<int> primera,segunda;
+    int n;
+
+    n=0;
+    cout<<"Ingrese elementos de la primera pila (-1 para terminar): "<<endl;
+    while(n!=-1){
+        cin>>n;
+        primera.push(n);
+    }
+
+    n=0;
+    cout<<endl<<"Ingrese elementos de la segunda pila (-1 para terminar): "<<endl;
+    while(n!=-1){
+        cin>>n;
+        segunda.push(n);
+    }
+
+    if(sonIguales(primera,segunda)){
+        cout<<endl<<"Son iguales";
+    }else{
+        cout<<endl<<"No son iguales";
+    }
+
     return 0;
+}
+bool sonIguales(Pila<int> primera,Pila<int> segunda){
+    while(true){
+        if(primera.esVacia()&&segunda.esVacia())return true;
+        if((primera.esVacia()&&!segunda.esVacia())||(segunda.esVacia()&&!primera.esVacia()))return false;
+        if(primera.pop()!=segunda.pop())return false;
+    }
 }
