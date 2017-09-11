d7736ee4a320e61d0c587d6f1f6e1ee66327edcd
FrancoBobadilla <30866005+FrancoBobadilla@users.noreply.github.com>
2017-09-11 19:12:55 -0300

    editado 7:12 10/9/2017

--- Ej-04/main.cpp
+++ Ej-04/main.cpp
@@ -1,33 +1,13 @@
 #include <iostream>
 #include "../Pila/Pila.h"
 
-
 using namespace std;
-
-bool comprobar(Pila<char> *, char);
-
-int main() {
-    Pila<char> pila;
-    string formula;
-    cout << endl << "Ingrese forumla a comprobar ('~' para salir)" << endl;
-    cin >> formula;
-    int fin = formula.length();
-    pila.push('~');
-    for (int i = 0; i < fin; i++) {
-        if (formula[i] == '~')return 1;
-        pila.push(formula[i]);
-    }
-    if (comprobar(&pila, '~')) cout << endl << "Formula balanceada" << endl;
-    else cout << endl << "Formula desbalanceada" << endl;
-    main();
-    return 0;
-}
 
 bool comprobar(Pila<char> *pila, char hasta) {
     while (!pila->esVacia()) {
         char caracter = pila->pop();
         if (caracter == hasta)return true;
-        if(caracter=='('||caracter=='['||caracter=='{'||caracter=='~')return false;
+        if (caracter == '(' || caracter == '[' || caracter == '{' || caracter == '~')return false;
         switch (caracter) {
             case ')': {
                 if (!comprobar(pila, '('))return false;
@@ -46,4 +26,21 @@ bool comprobar(Pila<char> *pila, char hasta) {
         }
     }
     return false;
+}
+
+int main() {
+    Pila<char> pila;
+    string formula;
+    cout << endl << "Ingrese forumla a comprobar ('~' para salir)" << endl;
+    cin >> formula;
+    long fin = formula.length();
+    pila.push('~');
+    for (int i = 0; i < fin; i++) {
+        if (formula[i] == '~')return 1;
+        pila.push(formula[i]);
+    }
+    if (comprobar(&pila, '~')) cout << endl << "Formula balanceada" << endl;
+    else cout << endl << "Formula desbalanceada" << endl;
+    main();
+    return 0;
 }
